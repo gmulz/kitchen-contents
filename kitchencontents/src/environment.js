d806ace12d6fbd352f11ddb1db5b6ec342d05e62
Grant Mulitz <themulitzer@gmail.com>
2022-01-09 22:32:48 -0500

    Adding server setup files

--- kitchencontents/src/environment.js
+++ kitchencontents/src/environment.js
@@ -1,1 +1,1 @@
-export const apiURL = 'http://localhost:8000/kitchen'
+export const apiURL = 'http://localhost:8001/kitchen'
---
+++ server/server.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+pip install -r requirements.txt
+
+sudo apt-get install postgresql
+
+sudo -u postgres psql -c "CREATE USER kitchenadmin WITH PASSWORD 'asdfghjkl';"
+sudo -u postgres psql -c "ALTER USER kitchenadmin WITH SUPERUSER CREATEDB CREATEROLE;"
+sudo -u postgres psql -c "CREATE DATABASE kitchendb OWNER  kitchenadmin"
+
+python ./kitchen/manage.py migrate
+
+python ./kitchen/manage.py loaddata ./kitchen/kitchenapp/fixtures/fixtures.json
+
+python ./budgettrackerbackend/manage.py runserver 0.0.0.0:8001
